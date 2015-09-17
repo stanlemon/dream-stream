c1ee36858cfa8a55764e09b69340733ee322324d
Stan Lemon <slemon@salesforce.com>
2015-09-17 08:29:47 -0700

    Add curl script for executing the demo.

---
+++ dream-stream-demo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Viewed%20a%20webpage\",\"email\":\"bertha@fcof.net\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Opened%20an%20email\",\"email\":\"barr_tim@grandhotels.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Clicked%20on%20a%20link\",\"email\":\"bond_john@grandhotels.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Opened%20an%20email\",\"email\":\"kakin@athenahome.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Unsubscribed\",\"email\":\"lboyle@uog.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Clicked%20on%20a%20link\",\"email\":\"bblair@abankingco.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Viewed%20a%20webpage\",\"email\":\"likeb@metro.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Opened%20an%20email\",\"email\":\"ldcruz@uog.com\"%7D"
+
+sleep 2;
+
+curl "https://dreamforce-real-time-stream.herokuapp.com/send?message=%7B\"event\":\"Clicked%20on%20a%20link\",\"email\":\"asong@uog.com\"%7D"
